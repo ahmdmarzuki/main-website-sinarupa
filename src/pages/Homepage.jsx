a5816af9f61bca8316cc8af40db537b5aa23daf9
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-06-19 23:50:16 +0700

    add major section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import HomeSection from "../sections/HomeSection";
 import AboutSection from "../sections/AboutSection";
+import MajorSection from "../sections/MajorSection";
 
 const Homepage = () => {
   return (
     <div>
       <HomeSection />
       <AboutSection />
+      <MajorSection />
       {/* <Link to="/upload">
         <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
           upload
