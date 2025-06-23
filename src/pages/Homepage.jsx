fbeb5dc494e47e22373ee17dec2732e8c4efefe8
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-06-23 13:21:40 +0700

    add bg

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,17 +9,25 @@ import ChromagiaEventSection from "../sections/ChromagiaEventSection";
 import LoadingSpinner from "../components/LoadingSpinner";
 import Navbar from "../components/Navbar";
 
+import bgDesktop from "/images/bg_desktop.png";
+import bgMobile from "/images/bgMobileRevisi.jpg";
+import { useMediaQuery } from "../useMediaQuery";
+
 const Homepage = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
-    <div>
+    <div
+      className="min-h-screen bg-cover"
+      style={{ backgroundImage: `url(${isMobile ? bgMobile : bgDesktop})` }}
+    >
       <Navbar />
-      <HomeSection />
-      <AboutSection />
+      {/* <HomeSection />
+      <AboutSection /> */}
       <MajorSection />
       <EventPathSection />
       {/* <ChromagiaEventSection /> */}
       <FaqSection />
-      <LoadingSpinner />
+      {/* <LoadingSpinner /> */}
       {/* <Link to="/upload">
         <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
           upload
