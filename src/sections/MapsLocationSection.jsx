7e902bb94f45f5d430b9f38342ce104753aa8c86
anastaciuh <anastaciaputri05@gmail.com>
2025-06-26 18:49:23 +0700

    p

--- src/sections/MapsLocationSection.jsx
+++ src/sections/MapsLocationSection.jsx
@@ -6,12 +6,12 @@ const MapsLocationSection = () => {
       <div className="flex flex-col items-center justify-center relative ml-10">
         <h1 className="text-5xl font-bold text-[#5740d2] font-oddval mb-4">Flow</h1>
         <div className="relative w-full md:w-1/3 flex flex-col md:flex-row items-center justify-center gap-10 mr-10">
-          <img src="/images/Flow1.png" className="w-11/12 md:w-full" alt="Flow Pejalan Kaki"/>
-          <img src="/images/Flow2.png" className="w-11/12 md:w-full" alt="Flow Kendaraan"/>
+          <img src=""/>
+          <img src=""/>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center relative mr-20n mt-20">
-        <h1 className="text-4xl font-bold text-[#5740d2] font-oddval mb-4">Peta</h1>
+        <h1 className="text-4xl font-bold text-[#5740d2] font-oddval mb-4"></h1>
         <img src="/images/PetaGSG.png" className="w-10/12 md:w-1/2" alt="Peta GSG"/>
       </div>
       <div className="flex flex-col items-center justify-center relative">
