27c85bdece16fe553099693e480f0995a7438ff7
anastaciuh <anastaciaputri05@gmail.com>
2025-06-26 20:01:52 +0700

    abtt

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -6,8 +6,8 @@ import Footer from "./Footer";
 const AboutSection = () => {
   return (
     <>
-      <div className="flex flex-col md:items-center justify-start lg:justify-center relative -pt-2 lg:-mt-[02vh] w-full lg:px-16 gap-2 lg:gap-8">
-        <h1 className="text-4xl font-bold text-[#FDE36E] font-oddval text-center -mt-[40vh] lg:-mt-[30vh]">
+      <div className="flex flex-col md:items-center justify-start lg:justify-center relative -pt-2 lg:mt-[20vh] w-full lg:px-16 gap-2 lg:gap-8">
+        <h1 className="text-4xl font-bold text-[#FDE36E] font-oddval text-center -mt-[20vh] lg:-mt-[40vh]">
           ABOUT
         </h1>
         <div className="relative">
