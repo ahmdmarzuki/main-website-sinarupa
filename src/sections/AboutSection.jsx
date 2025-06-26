49ffda060cee001cbf3b4fb370f04a55f43b96ac
luvinscha <luvinscha.dinasty@gmail.com>
2025-06-26 19:49:05 +0700

    bb

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
   return (
     <>
       <div className="flex flex-col md:items-center justify-start lg:justify-center relative -pt-2 lg:-mt-[02vh] w-full lg:px-16 gap-2 lg:gap-8">
-        <h1 className="text-4xl font-bold text-[#FDE36E] font-oddval text-center -mt-[10vh] lg:-mt-[15vh]">
+        <h1 className="text-4xl font-bold text-[#FDE36E] font-oddval text-center -mt-[10vh] lg:mt-20">
           ABOUT
         </h1>
         <div className="relative">
